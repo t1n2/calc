5912cffd2de09e5c9eb0695c0fcb7dcc49430366
t1n2 <46750536+t1n2@users.noreply.github.com>
2020-09-27 17:43:42 +0300

    Исправлен баг с кликом, добавлен код для '='

--- script.js
+++ script.js
@@ -3,38 +3,42 @@
 const inputArea = document.querySelector('.calc__input');
 const result = document.querySelector('.calc__result');
 const btn = document.querySelectorAll('button');
-let num = [];
+let arr = [];
 let temp = 0;
 
-addEventListener('click', (e) => {
-	if (e.target.value === 'c') {
-		inputArea.value = '';
-		result.innerHTML = '&nbsp;';
-        num = [];
-	 } else if (e.target.value === '+') {
-		result.innerHTML += e.target.value;
-		for (var i = 0; i < num.length; i++) {
-			if (num.length >= 2) {
-				temp += num[i];
-				inputArea.value = temp;
-			}
-		};
-		temp = 0;
-	} else if (e.target.value === '=') {
-		result.innerHTML += e.target.value;
-	} else {
-		num.push(+(e.target.value));
-		inputArea.value = e.target.value;
-		result.innerHTML += e.target.value;
+btn.forEach(item => {
+	item.addEventListener('click', (e) => {
+		if (e.target.value === 'c') {
+			inputArea.value = '';
+			result.innerHTML = '&nbsp;';
+	        arr = [];
+		 } else if (e.target.value === '+') {
+			result.innerHTML += e.target.value;
+			for (var i = 0; i < arr.length; i++) {
+				if (arr.length >= 2) {
+					temp += arr[i];
+					inputArea.value = temp;
+				}
+			};
+			temp = 0;
+		} else if (e.target.value === '=') {
+			result.innerHTML += e.target.value;
+			for (var i = 0; i < arr.length; i++) {
+				if (arr.length >= 2) {
+					temp += arr[i];
+					inputArea.value = temp;
+				}
+			};
+			arr.push(+(inputArea.value));
+			arr.splice(0, arr.length - 1);
+			result.innerHTML = '&nbsp;';
+			temp = 0;
+			console.log(arr);
+		} else {
+			arr.push(+(e.target.value));
+			inputArea.value = e.target.value;
+			result.innerHTML += e.target.value;
+			console.log(arr);
 	};
-
-	
-
-    
-	// 	for (var i = 0; i < inputArea.value.length; i++) {
-	// 		console.log(typeof(inputArea.value[i]));
-	// 		if (typeof(inputArea.value[i]) === Number) {
-	// 			arr.push(inputArea.value[i]);	
-	// 	}
-	// };
+	});
 });
